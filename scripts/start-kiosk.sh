73a102755790d7e02b34681a693407a3fcfc62e3
Jimmy Smith <jimmy.smith@compuproit.com>
2026-08-24 13:41:51 -0500

    Improve Chromium kiosk startup

--- scripts/start-kiosk.sh
+++ scripts/start-kiosk.sh
@@ -10,9 +10,11 @@ pkill chromium 2>/dev/null
 unclutter -idle 1 &
 
 exec chromium \
-  --user-data-dir=/home/user/.config/cpit-signage-chromium \
+  --user-data-dir="${HOME}/.config/cpit-signage-chromium" \
   --kiosk \
+  --start-fullscreen \
   --no-first-run \
+  --password-store=basic \
   --disable-session-crashed-bubble \
   --disable-infobars \
   --noerrdialogs \
